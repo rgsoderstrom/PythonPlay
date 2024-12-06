908a980e19329c94a26843106c4c70260d400ac6
Randy Soderstrom <rgsoderstrom@gmail.com>
2024-12-06 13:15:06 -0800

    Initial impound of Python tutorials & examples

---
+++ PythonApplication1/Example1.py
@@ -0,0 +1,16 @@
+import sys
+
+from math import cos, radians
+
+
+print ("Hello person")
+name = input('What is your name? ')
+print ('Hi, %s.' % name)
+
+print ("Welcome to VS");
+
+for i in range (20):
+    print (cos (radians (i)));
+
+
+
---
+++ PythonApplication1/Example3.py
@@ -0,0 +1,14 @@
+
+# Tutorial 5, Installing Packages
+
+
+from math import radians
+import numpy as np                # installed with matplotlib
+import matplotlib.pyplot as plt
+
+def main():
+   x = np.arange(0, radians(1800), radians(12))
+   plt.plot(x, np.cos(x), 'r')
+   plt.show()
+
+main()
